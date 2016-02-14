5fb137c58feb330d92a30144d9a838b7309276b6
oscar <oscaroceguera@gmail.com>
2016-02-14 16:39:50 -0700

    youtube api search begining use

--- src/redux-simple-starter-udemy/index.js
+++ src/redux-simple-starter-udemy/index.js
@@ -1,17 +1,30 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import you from 'youtube-api-search'
-
+import YTSearch from 'youtube-api-search'
 import SearchBar from './components/serach-bar.js'
-
 const API_KEY = 'AIzaSyBVdcToUX5OPN5-bnzXz3V0xMK7uO2p7No'
 
-const App = () => {
-	return (
-		<div>
-			<SearchBar />
-		</div>
-	)
+class App extends React.Component {
+
+	constructor(props){
+
+		super(props)
+
+		this.state = { videos: [] }
+
+		YTSearch({ key: API_KEY, term: 'surfboards' }, function(data){
+			console.log(data);
+		})
+
+	}
+
+	render(){
+		return (
+			<div>
+				<SearchBar />
+			</div>
+		)
+	}
 }
 
 ReactDOM.render(
